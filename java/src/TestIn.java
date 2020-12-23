d6067eda07bfd85c489f4aea8fb94351b3951283
leiyu <1549103271@qq.com>
2020-12-23 22:05:08 +0800

    java.test

---
+++ java/src/TestIn.java
@@ -0,0 +1,6 @@
+
+public interface TestIn {
+   int ID = 1;
+   
+   void test();
+}
---
+++ java/src/TestInImpl.java
@@ -0,0 +1,13 @@
+
+public class TestInImpl implements TestIn,TestIn1{
+
+	public void test() {
+		
+	}
+
+	public void test1() {
+		
+	}
+ 
+	
+}
