dc50749ec31057fc314bc5b7212617a7a0dd1db2
leiyu <1549103271@qq.com>
2021-01-08 22:17:40 +0800

    fight

--- Cocos/fight/fight/assets/script/enemy.js
+++ Cocos/fight/fight/assets/script/enemy.js
@@ -1,7 +1,7 @@
 const State = {
-    stand:1,
-    attack:2,
-    hurt:3,
+    stand: 1,
+    attack: 2,
+    hurt: 3,
 };
 cc.Class({
     extends: cc.Component,
@@ -10,29 +10,28 @@ cc.Class({
 
     },
 
-    onLoad(){
+    onLoad() {
         this.hp = 5;
         this.isHit = false;
         this.anima = 'idle';
         this.ani = this.node.getChildByName('body').getComponent(cc.Animation);
         this.rb = this.node.getComponent(cc.RigidBody);
         this._speed = 80;
-        this.sp = cc.v2(0 , 0);
+        this.sp = cc.v2(0, 0);
         this.tt = 0;
-        this.heroState = State.stand;
         this.enemyState = State.stand;
-        this.ani.on('finished',(e,data) => {
-            if(data.name == 'hurt'){
+        this.ani.on('finished', (e, data) => {
+            if (data.name == 'hurt') {
                 this.hp--;
                 this.isHit = false;
-                this.heroState = State.stand;
+                this.enemyState = State.stand;
 
-                if(this.hp == 0){
-                this.node.destroy();
-                } 
-            }else if(data.name == 'attack'){
-                this.ani.play('idle');
-                this.heroState = State.stand;
+                if (this.hp == 0) {
+                    this.node.destroy();
+                }
+            } else if (data.name == 'attack') {
+                this.setAni('idle');
+                this.enemyState = State.stand;
             }
         });
 
@@ -41,85 +40,104 @@ cc.Class({
 
         this.playerNode = cc.find('Canvas/bg/hero');
 
-    // setInterval(() => {
-    //     this.moveLeft = !this.moveLeft;
-    //     this.moveRight = !this.moveRight;
-    // },1000);        //隔一秒换个方向
+        // setInterval(() => {
+        //     this.moveLeft = !this.moveLeft;
+        //     this.moveRight = !this.moveRight;
+        // },1000);        //隔一秒换个方向
     },
-   hurt(){
-    if(this.isHit)return;
-    this.isHit = true;
-    this.enemyState = State.hurt;
-    this.lv = this.rb.linearVelocity;
-    this.lv.x = 0;
-    this.rb.linearVelocity = this.lv;
-    this.ani.play('hurt');
-   },
-
-   setAni(anima){
-    if(this.anima == anima)return;
-
-    this.anima = anima;
-    this.ani.play(anima);
-},
-
-enemyAction(tt){
-    let p_pos = this.playerNode.position;
-    let e_pos = this.node.position;
-    console.log(e_pos);
-    let dis = cc.Vec2.distance(e_pos,p_pos);
-    
-    if(dis <= 30){
-        console.log("攻击");
-    }else if(dis <= 150){
-        console.log("追击");
-    }else{
-        console.log("静止");
-    }
-   },
-
-attack(){
-    this.setAni('attack');
-    this.lv = this.rb.linearVelocity;
-    this.lv.x = 0;
-    this.rb.linearVelocity = this.lv;
-},
-move(){
-    let scaleX =Math.abs(this.node.scaleX);
-    this.lv = this.rb.linearVelocity;
-    if(this.moveLeft){
-        this.sp.x = -1;
-        this.node.scaleX = -scaleX;
-        this.setAni('run');
-    }else if (this.moveRight){
-        this.sp.x = 1
-        this.node.scaleX = scaleX;
-        anima = 'run';
-        this.setAni('run');
-    }else{
-        this.sp.x = 0;
-        this.setAni('idle');
-    }
-    if(this.sp.x){
-        this.lv.x = this.sp.x * this._speed;
-    }else{
+    hurt() {
+        if (this.isHit) return;
+        this.isHit = true;
+        this.enemyState = State.hurt;
+        this.lv = this.rb.linearVelocity;
         this.lv.x = 0;
-    }
-    this.rb.linearVelocity = this.lv;
+        this.rb.linearVelocity = this.lv;
+        this.setAni('hurt');
+    },
 
-},
-   update (dt){   
-    //状态切换
-    this.tt += dt;
-    if(this.tt >= 0.3 && this.enemyState == State.stand){
-        this.enemyAction(dt);
-        this.tt = 0;
-    }
-   
-    if (this.heroState == State.attack){
-       this.attack(); //攻击
-    }else if(this.heroState == State.stand){
-        this.move(); //移动
-    }
-},
+    setAni(anima) {
+        if (this.anima == anima) return;
+
+        this.anima = anima;
+        this.ani.play(anima);
+    },
+
+    enemyAction(tt) {
+        let p_pos = this.playerNode.position;
+        let e_pos = this.node.position;
+        let dis = cc.Vec2.distance(e_pos, p_pos);
+
+        if (dis <= 30) {
+            console.log("攻击");
+
+            this.moveLeft = false;
+            this.moveRight = false;
+
+            this.enemyState = State.attack;
+
+        } else if (dis <= 150) {
+            console.log("追击");
+
+            let v = p_pos.sub(e_pos);
+
+            if (v.x < 0) {
+                this.moveLeft = true;
+                this.moveRight = false;
+            } else {
+                this.moveLeft = false;
+                this.moveRight = true;
+            }
+            this.enemyState = State.stand;
+        } else {
+            console.log("静止");
+            this.moveLeft = false;
+            this.moveRight = false;
+            this.enemyState = State.stand;
+        }
+    },
+
+    attack() {
+        this.setAni('attack');
+        this.lv = this.rb.linearVelocity;
+        this.lv.x = 0;
+        this.rb.linearVelocity = this.lv;
+    },
+    move() {
+        let scaleX = Math.abs(this.node.scaleX);
+        this.lv = this.rb.linearVelocity;
+        if (this.moveLeft) {
+            this.sp.x = -1;
+            this.node.scaleX = -scaleX;
+            this.setAni('run');
+        } else if (this.moveRight) {
+            this.sp.x = 1
+            this.node.scaleX = scaleX;
+            // anima = 'run';
+            this.setAni('run');
+        } else {
+            this.sp.x = 0;
+            this.setAni('idle');
+        }
+        if (this.sp.x) {
+            this.lv.x = this.sp.x * this._speed;
+        } else {
+            this.lv.x = 0;
+        }
+        this.rb.linearVelocity = this.lv;
+
+    },
+    update(dt) {
+        //状态切换
+        this.tt += dt;
+        if (this.tt >= 0.3 && this.enemyState == State.stand) {
+            this.enemyAction(dt);
+            this.tt = 0;
+        }
+
+        if (this.enemyState == State.attack) {
+            this.attack(); //攻击
+        } else if (this.enemyState == State.stand) {
+            this.move(); //移动
+        }
+    },
 });
